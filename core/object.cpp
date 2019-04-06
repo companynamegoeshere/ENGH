8bcead285f3fafc68303bc15f845205d08f90dd8
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2019-04-05 23:42:30 -0300

    Project base

---
+++ core/object.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vinic on 4/2/2019.
+//
+
+#include "object.hpp"
---
+++ core/object.hpp
@@ -0,0 +1,14 @@
+//
+// Created by vinic on 4/2/2019.
+//
+
+#ifndef ENGH_OBJECT_HPP
+#define ENGH_OBJECT_HPP
+
+
+class Object {
+
+};
+
+
+#endif //ENGH_OBJECT_HPP
