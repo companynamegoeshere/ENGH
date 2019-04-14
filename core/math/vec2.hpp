8ff2098299183dfc59be1907413f81801a47e020
rafael garibaldi <rafaelfkf12@gmail.com>
2019-04-14 02:34:23 -0300

    Added Vec2 move/copy constructor

--- core/math/vec2.hpp
+++ core/math/vec2.hpp
@@ -4,9 +4,12 @@ template <typename T>
 class Vec2 {
 public:
 	T x, y;
+
 	Vec2();
 	Vec2(const T &x, const T &y);
-	// Vec2(const Vec2<T>& other);
+
+	Vec2(const Vec2<T>& other) = default;
+	Vec2(Vec2<T>&& other) = default;
 
 	Vec2<T>& operator+(const Vec2<T> &other) const;
 	Vec2<T>& operator+=(const Vec2<T> &other);
@@ -23,9 +26,6 @@ Vec2<T>::Vec2() : x(0), y(0) {}
 
 template <typename T>
 Vec2<T>::Vec2(const T &x, const T &y) : x(x), y(y) {}
-
-/* template <typename T>
-Vec2<T>::Vec2(const Vec2<T>& other) : x(other.x), y(other.y) {} */
 
 template<typename T>
 Vec2<T>& Vec2<T>::operator+(const Vec2<T> &other) const {
