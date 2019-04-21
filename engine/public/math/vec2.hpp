71a1e72b5e7330a1205f7d2674664f4dd847f9ff
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2019-04-21 03:56:10 -0300

    Vec2 updates
    Add scalar parameter to default constructor
    Fixed temporary object returns
    Add constexpr to methods and constructors

--- engine/public/math/vec2.hpp
+++ engine/public/math/vec2.hpp
@@ -1,91 +1,91 @@
 namespace ENGH::math {
 
-template<typename T>
-class TVec2 {
-public:
-    T x, y;
+	template<typename T>
+	class TVec2 {
+	public:
+		T x, y;
 
-    TVec2();
+		constexpr explicit TVec2(const T& value = 0);
 
-    TVec2(const T &x, const T &y);
+		constexpr TVec2(const T& x, const T& y);
 
-    TVec2(const TVec2<T> &other) = default;
+		constexpr TVec2(const TVec2<T>& other) = default;
 
-    TVec2(TVec2<T> &&other) noexcept = default;
+		constexpr TVec2(TVec2<T>&& other) noexcept = default;
 
-    TVec2<T> &operator+(const TVec2<T> &other) const;
+		constexpr TVec2<T> operator+(const TVec2<T>& other) const;
 
-    TVec2<T> &operator+=(const TVec2<T> &other);
+		constexpr TVec2<T>& operator+=(const TVec2<T>& other);
 
-    TVec2<T> &operator-(const TVec2<T> &other) const;
+		constexpr TVec2<T> operator-(const TVec2<T>& other) const;
 
-    TVec2<T> &operator-=(const TVec2<T> &other);
+		constexpr TVec2<T>& operator-=(const TVec2<T>& other);
 
-    TVec2<T> &operator*(const TVec2<T> &other) const;
+		constexpr TVec2<T> operator*(const TVec2<T>& other) const;
 
-    TVec2<T> &operator*=(const TVec2<T> &other);
+		constexpr TVec2<T>& operator*=(const TVec2<T>& other);
 
-    TVec2<T> &operator/(const TVec2<T> &other) const;
+		constexpr TVec2<T> operator/(const TVec2<T>& other) const;
 
-    TVec2<T> &operator/=(const TVec2<T> &other);
-};
+		constexpr TVec2<T>& operator/=(const TVec2<T>& other);
+	};
 
-template<typename T>
-TVec2<T>::TVec2() : x(0), y(0) {}
+	template<typename T>
+	constexpr TVec2<T>::TVec2(const T& value) : x(value), y(value) {}
 
-template<typename T>
-TVec2<T>::TVec2(const T &x, const T &y) : x(x), y(y) {}
+	template<typename T>
+	constexpr TVec2<T>::TVec2(const T& x, const T& y) : x(x), y(y) {}
 
-template<typename T>
-TVec2<T> &TVec2<T>::operator+(const TVec2<T> &other) const {
-    return TVec2<T>(x + other.x, y + other.y);
-}
+	template<typename T>
+	constexpr TVec2<T> TVec2<T>::operator+(const TVec2<T>& other) const {
+		return TVec2<T>(x + other.x, y + other.y);
+	}
 
-template<typename T>
-TVec2<T> &TVec2<T>::operator-(const TVec2<T> &other) const {
-    return TVec2<T>(x - other.x, y - other.y);
-}
+	template<typename T>
+	constexpr TVec2<T> TVec2<T>::operator-(const TVec2<T> & other) const {
+		return TVec2<T>(x - other.x, y - other.y);
+	}
 
-template<typename T>
-TVec2<T> &TVec2<T>::operator*(const TVec2<T> &other) const {
-    return TVec2<T>(x * other.x, y * other.y);
-}
+	template<typename T>
+	constexpr TVec2<T> TVec2<T>::operator*(const TVec2<T> & other) const {
+		return TVec2<T>(x * other.x, y * other.y);
+	}
 
-template<typename T>
-TVec2<T> &TVec2<T>::operator/(const TVec2<T> &other) const {
-    return TVec2<T>(x / other.x, y / other.y);
-}
+	template<typename T>
+	constexpr TVec2<T> TVec2<T>::operator/(const TVec2<T> & other) const {
+		return TVec2<T>(x / other.x, y / other.y);
+	}
 
-template<typename T>
-TVec2<T> &TVec2<T>::operator+=(const TVec2<T> &other) {
-    x += other.x;
-    y += other.y;
-    return *this;
-}
+	template<typename T>
+	constexpr TVec2<T>& TVec2<T>::operator+=(const TVec2<T> & other) {
+		x += other.x;
+		y += other.y;
+		return *this;
+	}
 
-template<typename T>
-TVec2<T> &TVec2<T>::operator-=(const TVec2<T> &other) {
-    x -= other.x;
-    y -= other.y;
-    return *this;
-}
+	template<typename T>
+	constexpr TVec2<T>& TVec2<T>::operator-=(const TVec2<T> & other) {
+		x -= other.x;
+		y -= other.y;
+		return *this;
+	}
 
-template<typename T>
-TVec2<T> &TVec2<T>::operator*=(const TVec2<T> &other) {
-    x *= other.x;
-    y *= other.y;
-    return *this;
-}
+	template<typename T>
+	constexpr TVec2<T>& TVec2<T>::operator*=(const TVec2<T> & other) {
+		x *= other.x;
+		y *= other.y;
+		return *this;
+	}
 
-template<typename T>
-TVec2<T> &TVec2<T>::operator/=(const TVec2<T> &other) {
-    x /= other.x;
-    y /= other.y;
-    return *this;
-}
+	template<typename T>
+	constexpr TVec2<T>& TVec2<T>::operator/=(const TVec2<T> & other) {
+		x /= other.x;
+		y /= other.y;
+		return *this;
+	}
 
-using Vec2 = TVec2<float>;
-using Vec2i = TVec2<int>;
+	using Vec2 = TVec2<float>;
+	using Vec2i = TVec2<int>;
 
 }//namespace ends here
 
