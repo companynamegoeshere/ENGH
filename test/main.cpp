297b72926458f2766378a0ecfba3825111931884
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2019-04-28 20:53:10 -0300

    Test file cleanup

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <math/vec2.hpp>
+#include <math/vec3.hpp>
 
 int main() {
-    ENGH::math::Vec2<int> a = {2,3};
-    std::cout << "Test " << a.x << '|' << a.y << std::endl;
+    ENGH::Math::Vec2 v2 = {1.0f, 1.0f};
+    ENGH::Math::Vec3 v3 = {1.0f, 1.0f, 1.0f};
 }
