eec56277514db423403b4979723cbd8aa2a48673
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2020-08-06 18:06:10 -0300

    Assert bug fix

--- engine/public/core/debug.hpp
+++ engine/public/core/debug.hpp
@@ -10,5 +10,5 @@ void Break();
 #ifdef ENGH_DEBUG
 #define ASSERT(x, msg) (void)((x) || (ENGH_ERROR("Assertion: failed ", #x, " at " __FILE__, "#", __LINE__), ::ENGH::Debug::Break(), true))
 #else
-#define ASSERT(x) (void)0
+#define ASSERT(x, msg) (void)0
 #endif
