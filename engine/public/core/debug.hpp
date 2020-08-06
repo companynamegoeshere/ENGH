9801270f6aaf63fdc45e2aa27075a54ba271a954
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2020-08-06 17:53:56 -0300

    Debug assert

--- engine/public/core/debug.hpp
+++ engine/public/core/debug.hpp
@@ -6,3 +6,9 @@ namespace ENGH::Debug {
 void Break();
 
 }
+
+#ifdef ENGH_DEBUG
+#define ASSERT(x, msg) (void)((x) || (ENGH_ERROR("Assertion: failed ", #x, " at " __FILE__, "#", __LINE__), ::ENGH::Debug::Break(), true))
+#else
+#define ASSERT(x) (void)0
+#endif
