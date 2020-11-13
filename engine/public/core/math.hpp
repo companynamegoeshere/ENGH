db58675811d24f556616909de94e81761a1e6a00
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2020-11-13 02:21:41 -0300

    Mathf lib

--- engine/public/core/math.hpp
+++ engine/public/core/math.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
 
 #include <core/data_types.hpp>
 #include <mathfu/glsl_mappings.h>
@@ -14,30 +16,33 @@ using Quat = mathfu::quat;
 
 using Mat4 = mathfu::mat4;
 
-const Vec3 VEC3_ZERO = Vec3(0, 0, 0);
-const Vec3 VEC3_ONE = Vec3(1, 1, 1);
-const Vec3 VEC3_RIGHT = Vec3(1, 0, 0);
-const Vec3 VEC3_UP = Vec3(0, 1, 0);
-const Vec3 VEC3_FORWARD = Vec3(0, 0, 1);
+const Vec3 VEC3_ZERO = Vec3(0.0f);
+const Vec3 VEC3_ONE = Vec3(1.0f);
+const Vec3 VEC3_MINUS_ONE = Vec3(-1.0f);
+
+const Vec3 VEC3_RIGHT = Vec3(1.0f, 0.0f, 0.0f);
+const Vec3 VEC3_UP = Vec3(0.0f, 1.0f, 0.0f);
+const Vec3 VEC3_FORWARD = Vec3(0.0f, 0.0f, 1.0f);
 
 constexpr double PI = 3.14159265358979323846;
-constexpr double PI2 = PI * 2;
-constexpr double HALF_PI = PI / 2;
+constexpr double PI2 = PI * 2.0;
+constexpr double HALF_PI = PI / 2.0;
 
-constexpr double DEGtoRAD = PI / 180;
-constexpr double RADtoDEG = 180 / PI;
+constexpr double DEGtoRAD = PI / 180.0;
+constexpr double RADtoDEG = 180.0 / PI;
 
 }
 
 namespace Mathf {
 
-constexpr float PI = 3.14159265358979323846;
-constexpr float PI2 = PI * 2;
-constexpr float HALF_PI = PI / 2;
+constexpr float PI = 3.14159265358979323846f;
+constexpr float PI2 = PI * 2.0f;
+constexpr float HALF_PI = PI / 2.0f;
 
-constexpr float DEGtoRAD = PI / 180;
-constexpr float RADtoDEG = 180 / PI;
-
-}
+constexpr float DEGtoRAD = PI / 180.0f;
+constexpr float RADtoDEG = 180.0f / PI;
 
 }
+
+}
+#pragma clang diagnostic pop
